c3959c7426f8ed7db194ea2e2a88eeee83495219
MAHMUDUL HASAN <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-01-13 05:44:54 +0000

    modify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: ['res.cloudinary.com'], // Add this line to configure the hostname
+    domains: ['res.cloudinary.com'],
     remotePatterns: [
       {
         protocol: 'https',
